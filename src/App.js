75050608075cab453a23fec63f8d1dc9637a4827
haseebulamin <haseebulamin07@gmail.com>
2024-01-16 22:05:48 +0500

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import './App.css';
 // import MagicPhoto from './components/MagicPhoto/MagicPhoto';
-import Menu from './components/Menu/Menu';
+// import Menu from './components/Menu/Menu';
 // import Axios from './components/Api_Intergration/Axios';
 // import Fatch from './components/Api_Intergration/Fatch';
 // import CustomHook from './components/CustomHook/CustomHook';
 // import UseReducer from './components/UseReducer/UseReducer';
 // import A from './components/UseContext/A';
-// import UseEffect from './components/UseEffect/UseEffect';
+import UseEffect from './components/UseEffect/UseEffect';
 // import UseState from './components/UseState/UseState';
 
 function App() {
   return (
    <>
    {/* <UseState/> */}
-   {/* <UseEffect/> */}
+   <UseEffect/>
    {/* <A/>  */}
    {/* <UseReducer/> */}
    {/* <CustomHook/> */}
    {/* <Fatch/> */}
    {/* <Axios/> */}
    {/* <MagicPhoto/> */}
-   <Menu/>
+   {/* <Menu/> */}
    </>
   );
 }
--- src/components/UseEffect/UseEffect.jsx
+++ src/components/UseEffect/UseEffect.jsx
@@ -16,14 +16,18 @@ export default function UseEffect() {
    }
 
    useEffect(() => {
-    alert("i am added")
-    
-  },[num])
+    alert("useefftect with add")
+  },[number]);
+
+   useEffect(() => {
+    alert("ueseffect with plus")
+  },[num]);
+
  
   return (
     <div className="counter bg-dark d-flex justify-content-center align-items-center ">
       <button type="button" class="btn btn-danger mx-2" onClick={()=> add1(1) }>Add {number} </button>
-      <button type="button" class="btn btn-danger mx-2" onClick={()=> add2(1) }>Add {num} </button>
+      <button type="button" class="btn btn-danger mx-2" onClick={()=> add2(1) }>Plus {num} </button>
       <button type="button" class="btn btn-success mx-2 px-3" onClick={clear}>Clear</button>
 
     </div>
