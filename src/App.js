b17a7f9cb949fba21045cae2fa0f8ae47c43e2a8
haseebulamin <haseebulamin07@gmail.com>
2024-01-17 21:44:08 +0500

    UseMemo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import UseMemo from './components/UseMemo/UseMemo';
 // import MagicPhoto from './components/MagicPhoto/MagicPhoto';
 // import Menu from './components/Menu/Menu';
 // import Axios from './components/Api_Intergration/Axios';
 // import Fatch from './components/Api_Intergration/Fatch';
 // import CustomHook from './components/CustomHook/CustomHook';
-import UseReducer from './components/UseReducer/UseReducer';
+// import UseReducer from './components/UseReducer/UseReducer';
 // import A from './components/UseContext/A';
 // import UseEffect from './components/UseEffect/UseEffect';
 // import UseState from './components/UseState/UseState';
@@ -15,7 +16,8 @@ function App() {
    {/* <UseState/> */}
    {/* <UseEffect/> */}
    {/* <A/>   */}
-   <UseReducer/>
+   {/* <UseReducer/> */}
+   <UseMemo/>
    {/* <CustomHook/> */}
    {/* <Fatch/> */}
    {/* <Axios/> */}
